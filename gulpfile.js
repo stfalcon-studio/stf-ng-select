685bc6609c5dc0d81ed8a340ec3498fda50f7eac
Andriy Syagrovskiy <andrij.sjagrovskij@stfalcon.com>
2017-07-10 17:56:14 +0300

    fix of types was added

--- gulpfile.js
+++ gulpfile.js
@@ -11,9 +11,9 @@ gulp.task('default', function () {
 
 
 gulp.task('compile', function () {
-  var tsProject = ts.createProject('tsconfig.json', { noImplicitAny: false, declaration: true });
+  var tsProject = ts.createProject('tsconfig.json', {  declaration: true, emitDecoratorMetadata: true });
   var tsResult = gulp.src(['src/app/stf-ng-select/**/*.ts'])
-        .pipe(inlineNg2Template({ base: '/src/app/stf-ng-select/stf-ng-select' }))
+        //.pipe(inlineNg2Template({ base: '/src/app/stf-ng-select/stf-ng-select' }))
         .pipe(tsProject());
   return merge([ // Merge the two output streams, so this task is finished when the IO of both operations is done. 
         tsResult.dts.pipe(gulp.dest('dist/lib')),
